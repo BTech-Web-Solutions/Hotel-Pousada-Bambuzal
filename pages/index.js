0aad41f38d9a69c44ca6adc65a3ec77e52769693
brodrigues0ll <bernardorg99@hotmail.com>
2023-01-21 00:38:10 -0300

    Chore: SEXTOU

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Navbar from '../src/components/Navbar';
 import ImageList from '../src/components/ImageList';
-import styled from 'styled-components';
 import Box from '@mui/material/Box';
 import Carrousel from '../src/components/Carrousel';
+import styled from 'styled-components';
 
 
 const theme = createTheme({});
@@ -19,8 +19,16 @@ export default function Index() {
       <ThemeProvider theme={theme}>
         <Navbar />
         <MainDiv>
-          <Carrousel />
-
+          <Box
+            maxHeight='lg'
+            sx={{
+              flexGrow: 1,
+              objectFit: 'cover',
+            }}
+          >
+            <Carrousel />
+          </Box>
+          <h1>Texto de teste</h1>
         </MainDiv>
       </ThemeProvider>
     </>
@@ -28,6 +36,5 @@ export default function Index() {
 }
 
 const MainDiv = styled.div`
-  height: 100vh;
 
 `;
