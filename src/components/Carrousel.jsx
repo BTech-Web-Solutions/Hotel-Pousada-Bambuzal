7e41639ec0101c3a400eb84dbee92594a4892a2c
brodrigues0ll <bernardorg99@hotmail.com>
2023-03-08 20:21:33 -0300

    Feat: Carrousel indicators display none

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,40 +1,38 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import styled from 'styled-components';
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import styled from "styled-components";
 
 const Carrousel = () => {
-
   return (
     <StyledCarrousel>
       <Carousel pause={false}>
         <Carousel.Item interval={5000}>
           <div
             style={{
-              color: 'white',
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              position: 'absolute',
-              zIndex: '1',
-              width: '100%',
-              height: '100%',
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              position: "absolute",
+              zIndex: "1",
+              width: "100%",
+              height: "100%",
             }}
           />
 
           <img
             src="https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/1.png?raw=true"
             alt="First slide"
-
           />
         </Carousel.Item>
 
         <Carousel.Item interval={5000}>
           <div
             style={{
-              color: 'white',
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              position: 'absolute',
-              zIndex: '1',
-              width: '100%',
-              height: '100%',
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              position: "absolute",
+              zIndex: "1",
+              width: "100%",
+              height: "100%",
             }}
           />
           <img
@@ -46,12 +44,12 @@ const Carrousel = () => {
         <Carousel.Item interval={5000}>
           <div
             style={{
-              color: 'white',
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              position: 'absolute',
-              zIndex: '1',
-              width: '100%',
-              height: '100%',
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              position: "absolute",
+              zIndex: "1",
+              width: "100%",
+              height: "100%",
             }}
           />
           <img
@@ -63,12 +61,12 @@ const Carrousel = () => {
         <Carousel.Item interval={5000}>
           <div
             style={{
-              color: 'white',
-              backgroundColor: 'rgba(0, 0, 0, 0.5)',
-              position: 'absolute',
-              zIndex: '1',
-              width: '100%',
-              height: '100%',
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              position: "absolute",
+              zIndex: "1",
+              width: "100%",
+              height: "100%",
             }}
           />
           <img
@@ -77,21 +75,27 @@ const Carrousel = () => {
           />
         </Carousel.Item>
       </Carousel>
-    </StyledCarrousel >
+    </StyledCarrousel>
   );
 };
 
 export default Carrousel;
 
-const StyledCarrousel = styled.div`  
-  h3, p, span, a {
+const StyledCarrousel = styled.div`
+  h3,
+  p,
+  span,
+  a {
     display: none;
   }
-  
+
   img {
     width: 100%;
     height: 100%;
     margin: auto;
-    
+  }
+
+  .carousel-indicators {
+    display: none;
   }
 `;
