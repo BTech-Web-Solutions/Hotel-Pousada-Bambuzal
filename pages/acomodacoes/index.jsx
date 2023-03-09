a9d08b200c4ab6477ec6d2b9896ce7b7260ff320
brodrigues0ll <bernardorg99@hotmail.com>
2023-03-09 01:21:13 -0300

    Feat: Começando Página de Acomodações

--- pages/acomodacoes/index.jsx
+++ pages/acomodacoes/index.jsx
@@ -1,11 +1,341 @@
-import React from 'react';
-import Navbar from '../../src/components/Navbar';
+import React from "react";
+import Image from "next/image";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+import Navbar from "../../src/components/Navbar";
+import Footer from "../../src/components/Footer";
+import suiteEntrada from "../../src/images/SuiteSimples/1.jpg";
 
 const index = () => {
   return (
     <>
       <Navbar />
-      <h1>Acomodações</h1>
+
+      <Box
+        sx={{
+          color: "white",
+          backgroundColor: "#1a1a1a",
+          padding: "2rem",
+          margin: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            },
+          }}
+        >
+          <Image
+            src={suiteEntrada}
+            alt="Suite Simples"
+            style={{
+              height: "30%",
+              width: "30%",
+              borderRadius: "1rem",
+            }}
+          />
+
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="h4">Suite Simples</Typography>
+
+            <Typography
+              variant="p"
+              sx={{
+                marginBottom: "1rem",
+              }}
+            >
+              Suítes com Vista para o rio, uma ótima opção para se ouvir as
+              águas do rio Sana.
+            </Typography>
+
+            <Typography variant="p">
+              <b>Configuração do Quarto:</b> <br />
+              1 Cama de Casal <br />
+              <b>Tamanho do Quarto:</b> 20m²
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          color: "white",
+          backgroundColor: "#1a1a1a",
+          padding: "2rem",
+          margin: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            },
+          }}
+        >
+          <Image
+            src={suiteEntrada}
+            alt="Suite Simples"
+            style={{
+              height: "30%",
+              width: "30%",
+              borderRadius: "1rem",
+            }}
+          />
+
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="h4">Suite Simples</Typography>
+
+            <Typography
+              variant="p"
+              sx={{
+                marginBottom: "1rem",
+              }}
+            >
+              Suítes com Vista para o rio, uma ótima opção para se ouvir as
+              águas do rio Sana.
+            </Typography>
+
+            <Typography variant="p">
+              <b>Configuração do Quarto:</b> <br />
+              1 Cama de Casal <br />
+              <b>Tamanho do Quarto:</b> 20m²
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          color: "white",
+          backgroundColor: "#1a1a1a",
+          padding: "2rem",
+          margin: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            },
+          }}
+        >
+          <Image
+            src={suiteEntrada}
+            alt="Suite Simples"
+            style={{
+              height: "30%",
+              width: "30%",
+              borderRadius: "1rem",
+            }}
+          />
+
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="h4">Suite Simples</Typography>
+
+            <Typography
+              variant="p"
+              sx={{
+                marginBottom: "1rem",
+              }}
+            >
+              Suítes com Vista para o rio, uma ótima opção para se ouvir as
+              águas do rio Sana.
+            </Typography>
+
+            <Typography variant="p">
+              <b>Configuração do Quarto:</b> <br />
+              1 Cama de Casal <br />
+              <b>Tamanho do Quarto:</b> 20m²
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          color: "white",
+          backgroundColor: "#1a1a1a",
+          padding: "2rem",
+          margin: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            },
+          }}
+        >
+          <Image
+            src={suiteEntrada}
+            alt="Suite Simples"
+            style={{
+              height: "30%",
+              width: "30%",
+              borderRadius: "1rem",
+            }}
+          />
+
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="h4">Suite Simples</Typography>
+
+            <Typography
+              variant="p"
+              sx={{
+                marginBottom: "1rem",
+              }}
+            >
+              Suítes com Vista para o rio, uma ótima opção para se ouvir as
+              águas do rio Sana.
+            </Typography>
+
+            <Typography variant="p">
+              <b>Configuração do Quarto:</b> <br />
+              1 Cama de Casal <br />
+              <b>Tamanho do Quarto:</b> 20m²
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          color: "white",
+          backgroundColor: "#1a1a1a",
+          padding: "2rem",
+          margin: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            },
+          }}
+        >
+          <Image
+            src={suiteEntrada}
+            alt="Suite Simples"
+            style={{
+              height: "30%",
+              width: "30%",
+              borderRadius: "1rem",
+            }}
+          />
+
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="h4">Suite Simples</Typography>
+
+            <Typography
+              variant="p"
+              sx={{
+                marginBottom: "1rem",
+              }}
+            >
+              Suítes com Vista para o rio, uma ótima opção para se ouvir as
+              águas do rio Sana.
+            </Typography>
+
+            <Typography variant="p">
+              <b>Configuração do Quarto:</b> <br />
+              1 Cama de Casal <br />
+              <b>Tamanho do Quarto:</b> 20m²
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          color: "white",
+          backgroundColor: "#1a1a1a",
+          padding: "2rem",
+          margin: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            },
+          }}
+        >
+          <Image
+            src={suiteEntrada}
+            alt="Suite Simples"
+            style={{
+              height: "30%",
+              width: "30%",
+              borderRadius: "1rem",
+            }}
+          />
+
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="h4">Suite Simples</Typography>
+
+            <Typography
+              variant="p"
+              sx={{
+                marginBottom: "1rem",
+              }}
+            >
+              Suítes com Vista para o rio, uma ótima opção para se ouvir as
+              águas do rio Sana.
+            </Typography>
+
+            <Typography variant="p">
+              <b>Configuração do Quarto:</b> <br />
+              1 Cama de Casal <br />
+              <b>Tamanho do Quarto:</b> 20m²
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Footer />
     </>
   );
 };
