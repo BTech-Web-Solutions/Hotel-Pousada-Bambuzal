85810522560e4260cc7e38c7f19110c20b23bec8
brodrigues0ll <bernardorg99@hotmail.com>
2023-03-09 18:34:03 -0300

    yes

--- pages/acomodacoes/index.jsx
+++ pages/acomodacoes/index.jsx
@@ -235,6 +235,90 @@ const index = () => {
             </CardActions>
           </Card>
         </Box>
+
+        <Box>
+          <Card
+            sx={{
+              maxWidth: "345px",
+              color: "white",
+              backgroundColor: "#1a1a1a",
+              padding: "2rem",
+              margin: "2rem",
+              borderRadius: "1rem",
+            }}
+          >
+            <CardActionArea>
+              <CardMedia
+                sx={{ borderRadius: "1rem" }}
+                component="img"
+                alt="Suíte Simples"
+                height="140"
+                image="https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/SuiteSimples/1.jpg?raw=true"
+                title="Suíte Simples"
+              />
+              <CardContent sx={{ bgcolor: "#1A1A1A", color: "white" }}>
+                <Typography gutterBottom variant="h4" component="h2">
+                  Suíte Simples
+                </Typography>
+                <Typography variant="body2" color="white" component="p">
+                  Suítes com Vista para o rio, uma ótima opção para se ouvir as
+                  águas do rio Sana. <br />
+                  <br />
+                  <b>Configuração do Quarto:</b> <br />
+                  1 Cama de Casal <br />
+                  <b>Tamanho do Quarto:</b> <br /> 20m²
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions sx={{ bgcolor: "#1A1A1A" }}>
+              <Button size="large" sx={{ color: "#eb5322" }}>
+                Ver Mais
+              </Button>
+            </CardActions>
+          </Card>
+        </Box>
+
+        <Box>
+          <Card
+            sx={{
+              maxWidth: "345px",
+              color: "white",
+              backgroundColor: "#1a1a1a",
+              padding: "2rem",
+              margin: "2rem",
+              borderRadius: "1rem",
+            }}
+          >
+            <CardActionArea>
+              <CardMedia
+                sx={{ borderRadius: "1rem" }}
+                component="img"
+                alt="Suíte Simples"
+                height="140"
+                image="https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/SuiteSimples/1.jpg?raw=true"
+                title="Suíte Simples"
+              />
+              <CardContent sx={{ bgcolor: "#1A1A1A", color: "white" }}>
+                <Typography gutterBottom variant="h4" component="h2">
+                  Suíte Simples
+                </Typography>
+                <Typography variant="body2" color="white" component="p">
+                  Suítes com Vista para o rio, uma ótima opção para se ouvir as
+                  águas do rio Sana. <br />
+                  <br />
+                  <b>Configuração do Quarto:</b> <br />
+                  1 Cama de Casal <br />
+                  <b>Tamanho do Quarto:</b> <br /> 20m²
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions sx={{ bgcolor: "#1A1A1A" }}>
+              <Button size="large" sx={{ color: "#eb5322" }}>
+                Ver Mais
+              </Button>
+            </CardActions>
+          </Card>
+        </Box>
       </Box>
 
       <Footer />
