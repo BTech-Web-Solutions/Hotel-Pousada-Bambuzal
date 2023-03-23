70a5d8fe9cd77730f9830e9b14f5271808abaadb
brodrigues0ll <bernardorg99@hotmail.com>
2023-03-23 16:48:16 -0300

    Update: Melhorando configuração do package.json

--- src/components/galeria/LobbyGallery.jsx
+++ src/components/galeria/LobbyGallery.jsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
@@ -26,14 +26,23 @@ export default function LobbyGallery() {
 
   return (
     <>
-      <ImageList sx={{ width: '100%', height: 500 }} variant="quilted" cols={4} rowHeight={121}>
+      <ImageList
+        sx={{ width: "100%", height: 500 }}
+        variant="quilted"
+        cols={4}
+        rowHeight={121}
+      >
         {itemData.map((item) => (
-          <ImageListItem key={item.img} cols={item.cols || 2} rows={item.rows || 1}>
+          <ImageListItem
+            key={item.img}
+            cols={item.cols || 2}
+            rows={item.rows || 1}
+          >
             <img
               {...srcset(item.img, 121, item.rows, item.cols)}
               alt={item.title}
               loading="lazy"
-              style={{ cursor: 'pointer' }}
+              style={{ cursor: "pointer" }}
               onClick={() => handleImageClick(item.img)}
             />
           </ImageListItem>
@@ -44,30 +53,30 @@ export default function LobbyGallery() {
         open={Boolean(selectedImage)}
         onClose={handleModalClose}
         sx={{
-          '& .MuiDialog-paper': {
+          "& .MuiDialog-paper": {
             p: 0,
             m: 0,
             maxWidth: {
-              xs: '80%',
-              sm: '80%',
-              md: '80%',
-              lg: '80%',
-              xl: '50%',
+              xs: "80%",
+              sm: "80%",
+              md: "80%",
+              lg: "80%",
+              xl: "50%",
             },
-            maxHeight: 'auto',
-            height: 'auto',
+            maxHeight: "auto",
+            height: "auto",
             width: {
-              xs: '100%',
-              sm: '100%',
-              md: '100%',
-              lg: '100%',
-              xl: '50%',
+              xs: "100%",
+              sm: "100%",
+              md: "50%",
+              lg: "50%",
+              xl: "60%",
             },
           },
         }}
       >
         <DialogContent>
-          <img src={selectedImage} style={{ maxWidth: '100%' }} />
+          <img src={selectedImage} style={{ maxWidth: "100%" }} />
         </DialogContent>
       </Dialog>
     </>
@@ -76,61 +85,61 @@ export default function LobbyGallery() {
 
 const itemData = [
   {
-    img: 'https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/1.png?raw=true',
-    title: 'Burger',
+    img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/1.png?raw=true",
+    title: "Burger",
   },
   {
     img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/2.png?raw=true",
-    title: 'Breakfast',
+    title: "Breakfast",
     rows: 2,
     cols: 2,
   },
   {
     img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/3.png?raw=true",
-    title: 'Camera',
+    title: "Camera",
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
     rows: 2,
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
   },
   {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
   },
   {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
     rows: 2,
     cols: 2,
   },
   {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
   },
   {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
   },
   {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
     cols: 4,
   },
 ];
