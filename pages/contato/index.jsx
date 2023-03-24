7936e291a11971f57053d46ca5b6d6f069e7e7bf
brodrigues0ll <bernardorg99@hotmail.com>
2023-03-24 06:16:19 -0300

    Feat: começando a página de contato css para responsivo

--- pages/contato/index.jsx
+++ pages/contato/index.jsx
@@ -7,16 +7,22 @@ import Navbar from "../../src/components/Navbar";
 const index = () => {
   return (
     <>
-      <Navbar position="0" />
+      <Navbar position="sticky" />
 
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
-          alignItems: "flex-end",
+          alignItems: {
+            xs: "center",
+            sm: "center",
+            md: "flex-end",
+            lg: "flex-end",
+            xl: "flex-end",
+          },
           justifyContent: "center",
           // bgcolor: "#505050",
-          height: "89vh",
+          height: "85vh",
           padding: "2rem",
         }}
       >
@@ -27,7 +33,13 @@ const index = () => {
             alignItems: "center",
             justifyContent: "center",
             bgcolor: "#181818",
-            width: "50%",
+            width: {
+              xs: "100%",
+              sm: "60%",
+              md: "50%",
+              lg: "50%",
+              xl: "50%",
+            },
             color: "#fff",
             padding: "2rem",
           }}
@@ -40,9 +52,17 @@ const index = () => {
               justifyContent: "center",
               alignItems: "center",
               fontWeight: "bold",
+              textAlign: "center",
+              fontSize: {
+                xs: "1.8rem",
+                sm: "2.5rem",
+                md: "2.5rem",
+                lg: "2.9rem",
+                xl: "2.5rem",
+              },
             }}
           >
-            Contato
+            Entre em contato conosco
           </Typography>
 
           <ContactInput id="nome" label="Nome" variant="outlined" />
