066577fd9d131ac480f75c3d5494747d83344edb
brodrigues0ll <bernardorg99@hotmail.com>
2023-03-25 13:54:43 -0300

    Feat: Página de Perguntas Frequentes

--- pages/faq/index.jsx
+++ pages/faq/index.jsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function SimpleAccordion() {
   return (
     <div>
-        <Typography>Perguntas frequentes</Typography>
+      <Typography>Perguntas frequentes</Typography>
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
