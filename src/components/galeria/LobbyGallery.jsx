06971238d6363756f462c2edf24d29ea2540c66c
QZBrainon <brainonqueiroz@gmail.com>
2023-10-16 16:08:06 -0300

    feat: edita links das imagens do lobby

--- src/components/galeria/LobbyGallery.jsx
+++ src/components/galeria/LobbyGallery.jsx
@@ -112,61 +112,22 @@ export default function LobbyGallery() {
 
 const itemData = [
   {
-    img: "https://media.githubusercontent.com/media/brodrigues0ll/Hotel-Pousada-Bambuzal/main/src/images/IMG-GIT-LFS/IMG-20230114-WA0020.jpg",
-    title: "Burger",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/Lobby/lobby_inside.jpg",
+    title: "Lobby",
   },
   {
-    img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/2.png?raw=true",
-    title: "Breakfast",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/Lobby/lobby_entrance.jpg",
+    title: "Entrance",
     rows: 2,
     cols: 2,
   },
   {
-    img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/3.png?raw=true",
-    title: "Camera",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/Lobby/lobby_inside_2.jpg",
+    title: "Lobby Inside",
   },
   {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/Lobby/lobby_inside_3.jpg",
+    title: "Lobby Close",
     cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    cols: 4,
   },
 ];
