fafbb6c7c31d12c2b8ef4278144d0b49c4b2def8
QZBrainon <brainonqueiroz@gmail.com>
2023-10-16 18:56:48 -0300

    feat: adiciona chale familia a galeria

--- src/components/galeria/ChalesFamilia.jsx
+++ src/components/galeria/ChalesFamilia.jsx
@@ -101,61 +101,41 @@ export default function ChalesFamilia() {
 
 const itemData = [
   {
-    img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/1.png?raw=true",
-    title: "Burger",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/1.jpg",
+    title: "Chale Familia",
   },
   {
-    img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/2.png?raw=true",
-    title: "Breakfast",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/chale_familia.jpg",
+    title: "Chale Familia 1",
     rows: 2,
     cols: 2,
   },
   {
-    img: "https://github.com/brodrigues0ll/Hotel-Pousada-Bambuzal/blob/main/src/images/Carrossel/3.png?raw=true",
-    title: "Camera",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/chale_familia_2.jpg",
+    title: "Chale Familia 2",
   },
   {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/chale_familia_3.jpg",
+    title: "Chale Familia 3",
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/chale_familia_4.jpg",
+    title: "Chale Familia 4",
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-    author: "@arwinneil",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/chale_familia_5.jpg",
+    title: "Chale Familia 5",
     rows: 2,
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/chale_familia_6.jpg",
+    title: "Chale Familia 6",
   },
   {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-    title: "Tomato basil",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-    title: "Bike",
-    cols: 4,
-  },
+    img: "https://media.githubusercontent.com/media/BTech-Web-Solutions/Hotel-Pousada-Bambuzal/main/src/images/ChaleFamilia/chale_familia_7.jpg",
+    title: "Chale Familia 7",
+  }
 ];
