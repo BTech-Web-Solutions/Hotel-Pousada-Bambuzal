c9618a8c220d9b2623a4128c62eed4cfa29b57d4
brodrigues0ll <bernardorg99@hotmail.com>
2023-10-24 11:10:07 -0300

    Ajuste do card de login em /eventos/admin

--- pages/eventos/admin/index.jsx
+++ pages/eventos/admin/index.jsx
@@ -5,40 +5,40 @@ export default function index() {
   return (
     <div>
       <Navbar />
-      <div class="login-container">
-        <form action="" class="form-login">
-          <ul class="login-nav">
-            <li class="login-nav__item active">
+      <div className="login-container">
+        <form action="" className="form-login">
+          <ul className="login-nav">
+            <li className="login-nav__item active">
               <a href="#">Sign In</a>
             </li>
           </ul>
-          <label htmlFor="login-input-user" class="login__label">
+          <label htmlFor="login-input-user" className="login__label">
             Username
           </label>
-          <input id="login-input-user" class="login__input" type="text" />
-          <label htmlFor="login-input-password" class="login__label">
+          <input id="login-input-user" className="login__input" type="text" />
+          <label htmlFor="login-input-password" className="login__label">
             Password
           </label>
           <input
             id="login-input-password"
-            class="login__input"
+            className="login__input"
             type="password"
           />
-          <label htmlFor="login-sign-up" class="login__label--checkbox">
+          <label htmlFor="login-sign-up" className="login__label--checkbox">
             <input
               id="login-sign-up"
               type="checkbox"
-              class="login__input--checkbox"
+              className="login__input--checkbox"
             />
             Keep me Signed in
           </label>
-          <button class="login__submit" disabled>
+          <button className="login__submit" disabled>
             Sign in
           </button>
         </form>
-        <a href="#" class="login__forgot">
+        {/* <a href="#" className="login__forgot">
           Forgot Password?
-        </a>
+        </a> */}
       </div>
     </div>
   );
