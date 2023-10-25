75df1f95a8c29515291b7b92cab1f80c71ad8536
brodrigues0ll <bernardorg99@hotmail.com>
2023-10-25 10:30:47 -0300

    Botão de LogOut

--- pages/eventos/index.jsx
+++ pages/eventos/index.jsx
@@ -42,10 +42,37 @@ export default function Events() {
 
   return (
     <>
-      <h1>Eventos</h1>
+      <h1
+        style={{
+          marginTop: "2rem",
+          marginBottom: "1rem",
+          textAlign: "center",
+          color: "#fff",
+        }}
+      >
+        Eventos
+      </h1>
       <button
         onClick={handleLogout}
-        style={{ marginTop: "1rem", padding: "0.5rem 1rem" }}
+        style={{
+          marginTop: "1rem",
+          padding: "0.5rem 1rem",
+          border: "none",
+          borderRadius: "0.5rem",
+          backgroundColor: "#fff",
+          color: "#000",
+          cursor: "pointer",
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+          fontSize: "1rem",
+          fontWeight: "bold",
+
+          ":hover": {
+            backgroundColor: "#000",
+            color: "#000",
+          },
+        }}
       >
         LogOut
       </button>
